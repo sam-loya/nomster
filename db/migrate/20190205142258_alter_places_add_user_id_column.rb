ca8cbcbd642c00a132f9c2781a793056507663aa
sam-loya <loyasamuel2018@gmail.com>
2019-02-08 17:25:52 +0000

    latest push

--- db/migrate/20190205142258_alter_places_add_user_id_column.rb
+++ db/migrate/20190205142258_alter_places_add_user_id_column.rb
@@ -1,7 +1,5 @@
-class AlterPlacesAddUserIdColumn < ActiveRecord::Migration[5.2]
+class AlterPlacesAddUserIdColumn < ActiveRecord::Migration[5.0]
   def change
-    add_column :places, :latitude, :float
-    add_column :places, :longitude, :float
-    
+    add_column :places, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2019_02_06_143235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
